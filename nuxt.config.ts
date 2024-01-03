a0db9219960d797293cd80ebd71cca7aab1d3dab
Daniel <daniel.pe.15y15@gmail.com>
2024-01-03 12:57:39 -0300

    change vuetify pluging dependence

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,16 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
   css: [
-    "~/assets/main.scss"
+    "~/assets/main.scss",
+    'vuetify/lib/styles/main.sass',
+    '@mdi/font/css/materialdesignicons.min.css',
   ],
 
-  modules: [
-    'vuetify-nuxt-module'
-  ]
+  build: {
+    transpile: ['vuetify'],
+  },
+
 })
